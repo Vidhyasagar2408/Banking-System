f388d486f85106c83e3a676f6aa7591195317ad7
vidhyasagar <vs1522250@gmail.com>
2026-08-17 00:27:20 +0530

    Banking-System

--- src/app.js
+++ src/app.js
@@ -8,6 +8,13 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Welcome to the Banking System API",
+  });
+});
+
 app.use("/api/auth", authRouter);
 app.use("/api/account", accountRouter);
 app.use("/api/transaction", trasactionRoutes);
